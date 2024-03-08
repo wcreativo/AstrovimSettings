8e973a436eaeaaee13272d7550359b2d72d59f19
wcreativo <w.kreativo@gmail.com>
2024-03-07 21:13:08 -0500

    Remove ChatGPT

--- plugins/user.lua
+++ plugins/user.lua
@@ -19,20 +19,6 @@ return {
     event = "User Astrofile",
   },
   {
-    "dreamsofcode-io/ChatGPT.nvim",
-    event = "VeryLazy",
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope.nvim",
-    },
-    config = function()
-      require("chatgpt").setup {
-        async_api_key_cmd = "pass show chatgpt",
-      }
-    end,
-  },
-  {
     "catppuccin/nvim",
     name = "catppuccin",
     opts = {
